187431c5ee40bfd8984462092d8d7d92d262845d
DylanArnold101 <league@WTS7.attlocal.net>
2018-07-23 15:02:44 -0700

    robot graffiti robot square

---
+++ src/RobotGraffiti.java
@@ -0,0 +1,23 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+	public static void main(String[] args) {
+		Robot joe = new Robot();
+		joe.penDown();
+		joe.setSpeed(100);
+		joe.turn(-90);
+		joe.move(200);
+		
+		joe.turn(90);
+		joe.move(200);
+		joe.turn(90);
+		joe.move(200);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
