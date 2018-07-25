bf8b8fd557c551236fe059e88922d6d8f0e1f0bb
komodochristopher <league@WTS7.attlocal.net>
2018-07-25 13:55:08 -0700

    greeter

--- src/Greeter.java
+++ src/Greeter.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 
 public class Greeter {
 public static void main(String[] args) {
-  JOptionPane.showInputDialog("whats your name")
-  JOptionPane.showMessage(null,"Hi jack"); 
+  String name=JOptionPane.showInputDialog("whats your name");
+  JOptionPane.showMessageDialog(null,"Hi "+ name); 
 }	
 }
 
